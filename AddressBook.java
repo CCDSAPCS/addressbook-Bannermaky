1a9e5ddb9616bde0c26993390ee61e832f109567
Bannermaky <bannermaky@countryday.net>
2017-04-07 11:10:21 -0400

    Update AddressBook.java

--- AddressBook.java
+++ AddressBook.java
@@ -3,23 +3,23 @@ import java.util.Scanner;
 
 public class AddressBook {
 
-    private Contact[] friends; // An array of Contacts - each stores info for one friend
-    private int numfriends; // Number of friends currently in AddressBook
+private Contact[] friends; // An array of Contacts - each stores info for one friend
+private int numfriends; // Number of friends currently in AddressBook
 
 	
-    // Create an empty AddressBook
-    public AddressBook() {
-		friends = new Contact[10];
-		numfriends = 0;
-    }
+// Create an empty AddressBook
+public AddressBook() {
+	friends = new Contact[10];
+	numfriends = 0;
+}
 	
 	
-    public void addContact(Contact c){
-	    friends[numfriends] = c;
-	    numfriends++;
-    }
+public void addContact(Contact c){
+	friends[numfriends] = c;
+	 numfriends++;
+}
 	
-    private int haveContact(String s){
+private int haveContact(String s){
 	for (int i=0; i<numfriends; i++){
 		if(friends[i].getName.equals(s)){
 		    return i;
@@ -28,13 +28,20 @@ public class AddressBook {
 		    return -1;
 		}
 	}
-    }
+}
+	    
+public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+        AddressBook redbook = new AddressBook();
+        menu();
+	int choice = scan.nextInt();
+}
 	
-     public static void menu(){
-		System.out.println("1.Add a new contact to your address book.");
-		System.out.println("2.Delete a contact from your address book.");
-		System.out.println("3.Print out the number of contacts you have.");
-		System.out.println("4.Print out information of all your contacts.");
-		System.out.println("5.Quit");
-		System.out.println("Enter your menu choice:");
-      }
+public static void menu(){
+	System.out.println("1.Add a new contact to your address book.");
+	System.out.println("2.Delete a contact from your address book.");
+	System.out.println("3.Print out the number of contacts you have.");
+	System.out.println("4.Print out information of all your contacts.");
+	System.out.println("5.Quit");
+	System.out.println("Enter your menu choice:");
+ }
